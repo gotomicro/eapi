bb31ee7ce8f3d28fe5c282d5e01ff771eb406251
duanlv <itsl@foxmail.com>
2023-01-16 16:41:09 +0800

    fix: trim space in comment

--- plugins/gin/testdata/server/frontend/src/types/types.ts
+++ plugins/gin/testdata/server/frontend/src/types/types.ts
@@ -1,8 +1,16 @@
+/*
+ * @description is a single URL parameter, consisting of a key and a value.
+ */
 export type Param = {
   Key?: string;
   Value?: string;
 }
 
+/*
+ * @description is a Param-slice, as returned by the router.
+ *	The slice is ordered, the first URL parameter is also the first slice value.
+ *	It is therefore safe to read values by the index.
+ */
 export type Params = Param[]
 
 export enum ErrCode {
@@ -18,18 +26,48 @@ export type Error = {
 }
 
 export type GoodsCreateReq = {
+  /*
+   * @description 封面图
+   */
   cover?: string;
+  /*
+   * @description 详情图
+   */
   images?: Image[];
+  /*
+   * @description 价格(分)
+   */
   price: number;
+  /*
+   * @description 商品描述
+   */
   subTitle?: string;
+  /*
+   * @description 商品标题
+   */
   title: string;
 }
 
 export type GoodsCreateRes = {
+  /*
+   * @description 测试引用第三方包
+   */
   Status?: Params;
+  /*
+   * @description 商品 GUID
+   */
   guid?: string;
+  /*
+   * @description 测试引用内置包类型
+   */
   raw?: any;
+  /*
+   * @description 测试循环引用
+   */
   selfRef?: SelfRefType;
+  /*
+   * @description 测试类型别名
+   */
   stringAlias?: string;
 }
 
@@ -46,7 +84,13 @@ export type GoodsInfoRes = {
   title?: string;
 }
 
+/*
+ * @description 商品图片
+ */
 export type Image = {
+  /*
+   * @description 图片链接
+   */
   url: string;
 }
 
